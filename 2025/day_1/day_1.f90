f6d006f1cab5cfd7f6d74224976e1ab817e74210
remie92 <chris@hupse.eu>
2026-03-10 20:19:55 +0100

    day 1 part one complete. Holy.

--- 2025/day_1/day_1.f90
+++ 2025/day_1/day_1.f90
@@ -2,18 +2,56 @@
 program test
     INTEGER :: io=2
     character(10) :: a
-    open(unit=io,file="input.txt",status="old",action="read")
+    character(1) :: dir
+    character(10) :: numStr
+    INTEGER :: num
+    INTEGER :: dial=50
+    INTEGER :: counter=0
+    open(unit=io,file="input.txt")
     do 
+        print * ,""
         read(io, *,IOSTAT = IOS) a
-        print *,IOS
+
+        dir=a (1:1)
+        numStr=a(2:10)
+        read(numStr,'(I10)') num
+        print *,dir,num
+
+        if (dir=="R") then
+            print *,"To The Right!"
+            dial=dial+num
+        end if
+        if (dir=="L") then
+            print *,"To The Left!"
+            dial=dial-num
+        end if
+
+        DO WHILE (dial<0)
+            dial=dial+100
+        end do
+        DO WHILE (dial>99)
+            dial=dial-100
+        end do
+
+        if (dial==0) then
+            counter=counter+1
+        end if
+
+
+        print *,"Dial:",dial,"Counter:",counter
+
+
         if (IOS==-1) then
             print *,"STOP"
             exit
         end if
-        print *, a
+
+
     end do
 
     close(io)
+
+    print *,counter
 
     
 end program test
--- 2025/day_1/run.sh
+++ 2025/day_1/run.sh
@@ -1,3 +1,4 @@
 gfortran ./day_1.f90 -o ./day_1
 ./day_1
+rm ./day_1
 #bash  ./2025/day_1/run.sh
